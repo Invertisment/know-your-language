96ea65ab63b73c73774fe9292550cb375c617f42
Invertisment <visiskaj.slaptaj@gmail.com>
2018-09-17 12:17:58 +0300

    initial

---
+++ README.md
@@ -0,0 +1,8 @@
+#Boilerplate detector
+
+This script shows how much boilerplate a programming language has 
+
+```bash
+go to your source directory
+$@ \"*.go\" > output-go.txt
+```
---
+++ know.sh
@@ -0,0 +1,9 @@
+
+if [ $# -eq 0 ]
+  then
+    echo "Usage:"
+    echo "go to your source directory"
+    echo "$@ \"*.go\" > output-go.txt"
+  fi
+
+find -name $1 | grep -v "#" | xargs cat | sed 's/^[\t ]*//g' | sed 's/[\t ]*$//g' | sort | uniq -c | sort -n
